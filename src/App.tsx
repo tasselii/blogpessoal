d0abeed607fd07eb7131456ae8065cdb9311e92d
Thiago Tasseli <thiagotasseli@outlook.com.br>
2025-06-25 09:11:47 -0300

    :sparkles: feat: cria componente de cadastro de tema com formulário e integração

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./assets/pages/home/home"
 import Footer from "./componentes/footer/Footer"
 import Navbar from "./componentes/navbar/Navbar"
+import ListaTemas from "./componentes/temas/listatemas/ListaTemas"
+import FormTema from "./componentes/temas/formtema/FormTema"
 import Cadastro from "./assets/pages/cadastro/Cadastro"
 import Login from "./assets/pages/login/Login"
 import { AuthProvider } from "./contexts/AuthContexts"
+import DeletarTema from "./componentes/temas/deletartema/DeletarTema"
 
 
 function App() {
@@ -19,6 +22,10 @@ function App() {
                   <Route path="/home" element={<Home />} />
                   <Route path="/cadastro" element={<Cadastro />} />
                   <Route path="/login" element={<Login />} />
+                  <Route path="/temas" element={<ListaTemas />} />
+                  <Route path="/cadastrartema" element={<FormTema />} />
+                  <Route path="/editartema/:id" element={<FormTema />} />
+                  <Route path="/deletartema/:id" element={<DeletarTema />} />
                 </Routes>
               </div>
             <Footer />
--- src/componentes/navbar/Navbar.tsx
+++ src/componentes/navbar/Navbar.tsx
@@ -24,12 +24,13 @@ function Navbar() {
 
                     <div className='flex gap-4'>
                         Postagens
-                        Temas
-                        Cadastrar tema
+                        <Link to='/temas' className='hover:underline'>
+                        Temas </Link>
+                        <Link to='/cadastrartema' className='hover:underline'>
+                        Cadastrar tema </Link>
                         Perfil
                         <Link to="/login" onClick={logout} className="hover-underline" >
-                        Sair
-                        </Link>
+                        Sair </Link>
                     </div>
                 </div>
             </div>
