ce29fc3e84d1d1ed7e8d7a583abf35b0a5738259
Thiago Tasseli <thiagotasseli@outlook.com.br>
2025-06-29 04:09:23 -0300

    :wrench: alterações no componente Footer - Navbar

--- src/componentes/footer/Footer.tsx
+++ src/componentes/footer/Footer.tsx
@@ -1,27 +1,38 @@
-import { LinkedinLogo, GithubLogo } from '@phosphor-icons/react'
+import { GithubLogo, LinkedinLogo } from '@phosphor-icons/react'
+import {  type ReactNode, useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
 
     let data = new Date().getFullYear()
 
-    return (
-        <>
-            <div className="flex justify-center bg-indigo-900 text-white">
+    const { usuario } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+            <div className="flex justify-center text-white bg-slate-800">
                 <div className="container flex flex-col items-center py-4">
-                    <p className='text-base font-bold'>
-                            © 2025 Thiago Tasseli. Todos os direitos reservados. | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Minhas Redes Sociais</p>
-                    <div className='flex gap-2'>
-                        <a href="https://www.linkedin.com/in/thiagotasseli-tech/" target='_blank'>
-                        <LinkedinLogo cursor="pointer" size={48} weight="bold" color="#0A66C2" />
+                    <p className="text-ml font-bold">© 2025 Thiago Tasseli. This site is <a className="underline hover:text-blue-800 " href="https://github.com/tasselii" target="_blank">open source</a>!</p>
+                    <p className="text-lg">Acesse as redes sociais</p>
+                    <div className="flex gap-2">
+                        <a href="" target="_blank" className="hover:text-blue-600 transform hover:scale-110 transition-transform duration-300">
+                            <LinkedinLogo size={48} weight="bold" />
                         </a>
-                        <a href="https://github.com/tasselii" target='_blank'>
-                        <GithubLogo href='https://github.com/tasselii' target="_blank" cursor="pointer" size={48} weight="bold" color="#171515" />    
-                        </a>
+                       <a href="https://github.com/tasselii" target="_blank" rel="noopener noreferrer" className="hover:text-black transform hover:scale-105 transition-transform duration-300">
+                       <GithubLogo size={40} weight="bold" className="border-5 m-1 rounded" />
+                       </a>
+                        
                     </div>
                 </div>
             </div>
+    )
+    }
+    return (
+        <>
+            { component }
         </>
     )
 }
--- src/componentes/navbar/Navbar.tsx
+++ src/componentes/navbar/Navbar.tsx
@@ -1,39 +1,50 @@
+import { type ReactNode, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom"
-import { AuthContext } from "../../contexts/AuthContexts"
-import { useContext } from "react"
+import { AuthContext } from "../../contexts/AuthContext";
+import { ToastAlerta } from "../../utils/ToastAlerta";
 
 function Navbar() {
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
-    const { handleLogout } = useContext(AuthContext)
+    const { usuario, handleLogout } = useContext(AuthContext)
 
     function logout() {
-    handleLogout()
-    alert("O usuário foi desconectado com sucesso!")
-    navigate("/")
-}
 
-    return (
-        <>
+        handleLogout()
+        ToastAlerta('O Usuário foi desconectado com sucesso!', 'info')
+        navigate('/')
+    }
+    
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
             <div className='w-full bg-indigo-900 text-white
                 flex justify-center py-4'>
-            
+
                 <div className="container flex justify-between text-lg">
-                    <Link to="/home" className="text-2xl font-bold"> Blog Pessoal </Link>
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-4'>
-                        Postagens
-                        <Link to='/temas' className='hover:underline'>
-                        Temas </Link>
-                        <Link to='/cadastrartema' className='hover:underline'>
-                        Cadastrar tema </Link>
-                        Perfil
-                        <Link to="/login" onClick={logout} className="hover-underline" >
-                        Sair </Link>
+                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                        <Link to='/perfil' className='hover:underline'>Perfil</Link>
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
                 </div>
             </div>
+
+        )
+
+    }
+
+    return (
+        <>
+            { component }
         </>
     )
 }
